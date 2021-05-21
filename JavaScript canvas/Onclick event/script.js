717ba425dd25aa7f29c0a6bbfb912da5b2564774
Faisal kalwar <faisalkalwar48@gmail.com>
2021-05-22 02:43:55 +0500

    fontawesome-complete

---
+++ JavaScript canvas/Onclick event/script.js
@@ -0,0 +1,9 @@
+function ChooseEasy(){
+    alert('Hello! This is level Easy.');
+}
+function ChooseMedium(){
+    alert('Hello! This is level Medium.')
+}
+function ChooseHard(){
+    alert('Hello! This is level Hard.')
+}
